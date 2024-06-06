9177a48d72d45a776f61224be13995b92b8ae92b
AlexDelei <alexdelei125@gmail.com>
2024-06-06 18:16:52 +0300

    summation of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+sum of floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    add all the numbers in the list
+
+    input_list -- list of floats
+    """
+
+    return (sum(input_list))
