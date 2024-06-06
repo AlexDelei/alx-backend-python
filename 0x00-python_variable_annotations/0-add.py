d08d2d05505a8e47c2af2d5044ce937fe8259c8a
AlexDelei <alexdelei125@gmail.com>
2024-06-06 16:00:59 +0300

    simple annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A simple type-annotated function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Sum of args
+
+    a -- first number of type float
+    b -- second number
+    """
+    return (a + b)
