0722882422fad1ff903a069edf5b9ecf4413e10d
AlexDelei <alexdelei125@gmail.com>
2024-06-06 16:11:26 +0300

    concatinate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+simple annotation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatinates two strings
+
+    str1 -- first string
+    str2 -- second string
+    """
+    return (str1 + str2)
