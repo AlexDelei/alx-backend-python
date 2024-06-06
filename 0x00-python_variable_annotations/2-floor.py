93f9dc5c13a28ef37344e4b9d5006be3d357771f
AlexDelei <alexdelei125@gmail.com>
2024-06-06 16:21:20 +0300

    Rounds Down

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import math
+"""
+type-annotated function
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Round down
+
+    n -- number to round down
+    """
+    return (math.floor(n))
