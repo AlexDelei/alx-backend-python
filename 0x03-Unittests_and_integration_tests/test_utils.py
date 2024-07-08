ef029df48462dcdc2fe0da9d8329cc5758252ffe
AlexDelei <alexdelei125@gmail.com>
2024-07-08 13:41:46 +0300

    Parameterizing a Unit test

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Parameterizing a unit test
+"""
+import unittest
+from parameterized import parameterized
+from typing import Mapping, Sequence
+access_nested_map = __import__('utils').access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Parameterize a unit test
+    """
+
+    @parameterized.expand([
+        ({'a': 1}, ['a'], 1),
+        ({"a": {"b": 2}}, ['a'], {"b": 2}),
+        ({"a": {"b": 2}}, ['a', 'b'], 2)
+    ])
+    def test_access_nested_map(self, nested_map: Mapping, keys: Sequence, exp):
+        """Parameterize a Unit test"""
+        self.assertEqual(access_nested_map(nested_map, keys), exp)
+
+
+if __name__ == '__main__':
+    unittest.main()
