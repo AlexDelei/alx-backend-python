4e5fa8ef716cf405c46e36371f3e654d9ee3c652
AlexDelei <alexdelei125@gmail.com>
2024-07-09 12:56:12 +0300

    The file should not be practically imported

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -142,7 +142,3 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             client.public_repos(license="apache-2.0"),
             self.apache2_repos
             )
-
-
-if __name__ == "__main__":
-    unittest.main()
