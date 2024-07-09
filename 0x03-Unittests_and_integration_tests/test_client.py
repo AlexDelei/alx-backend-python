d0eacf154dcbc9af376be27f1cfd2a8705d04d1d
AlexDelei <alexdelei125@gmail.com>
2024-07-09 08:36:50 +0300

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 Unittests for Client
 """
 import unittest
-from unittest.mock import patch, MagicMock, Mock
+from unittest.mock import PropertyMock, patch, MagicMock, Mock
 from parameterized import parameterized
 GithubOrgClient = __import__('client').GithubOrgClient
 
@@ -23,6 +23,22 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org, {'payload': True})
         mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{org_name}')
+
+    @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Mocking a Property
+        """
+        # defining the mocked payload
+        mock_payload = {'repos_url': "https://api.github.com/orgs/testorg/repos"}
+
+        # setting the return value of the mocked org property
+        mock_org.return_value = mock_payload
+
+        # Instatiating the client
+        client = GithubOrgClient('testorg')
+
+        # _public_repos_url returns the expected url
+        self.assertEqual(client._public_repos_url, "https://api.github.com/orgs/testorg/repos")
 
 
 if __name__ == "__main__":
